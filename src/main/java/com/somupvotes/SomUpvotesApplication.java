3001878e2c6956122915c48b45c66bdf1bec1ced
user <user@users-MacBook-Pro.local>
2020-07-28 17:18:01 +0100

    first commit

---
+++ src/main/java/com/somupvotes/SomUpvotesApplication.java
@@ -0,0 +1,13 @@
+package com.somupvotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomUpvotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SomUpvotesApplication.class, args);
+	}
+
+}
