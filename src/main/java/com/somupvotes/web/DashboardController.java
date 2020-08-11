043ba0cf0bbf4aeee4c4d9667a5a91e198e38169
M-Isak <mohamedisak.ali@gmail.com>
2020-08-11 14:34:09 +0100

    Adding Password Encoder

--- src/main/java/com/somupvotes/web/DashboardController.java
+++ src/main/java/com/somupvotes/web/DashboardController.java
@@ -1,13 +1,25 @@
 package com.somupvotes.web;
 
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DashboardController {
+	
+//	@Autowired
+//	private PasswordEncoder passwordEcoder;
 
- @GetMapping("/index")
+ @GetMapping("/")
     public String rootView(){
         return "index";
     }
+ 
+ @GetMapping("/dashboard")
+ public String dashboard(){
+	 
+	 return "dashboard";
+ }
+ 
 }
--- src/main/java/com/somupvotes/websecurity/webSecurityConfig.java
+++ src/main/java/com/somupvotes/websecurity/webSecurityConfig.java
@@ -1,19 +1,31 @@
 package com.somupvotes.websecurity;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class webSecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Bean
+    public PasswordEncoder getPasswordEncoder() {
+		
+		return new BCryptPasswordEncoder();
+	}
+	
+	
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		// TODO Auto-generated method stub
 		auth.inMemoryAuthentication()
+		    .passwordEncoder(getPasswordEncoder())
 		    .withUser("mohamedisak.ali@gmail.com")
-		    .password("test123")
+		    .password(getPasswordEncoder().encode("test123"))
 		    .roles("USER");
 	}
 	
@@ -23,11 +35,11 @@ public class webSecurityConfig extends WebSecurityConfigurerAdapter{
 		http.csrf().disable()
 		     .authorizeRequests()
 		     .antMatchers("/").permitAll()
-		     .antMatchers("/login").permitAll()
 		     .anyRequest().hasRole("USER").and()
 		     .formLogin()
 		     .loginPage("/login")
 		     .permitAll()
+		     .defaultSuccessUrl("/dashboard")
 		     .and()
 		     .logout()
 		     .logoutUrl("/logout")
