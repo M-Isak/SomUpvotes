37c9e3b92f399b3e9dd91d3107d36318dff9b92d
M-Isak <mohamedisak.ali@gmail.com>
2020-08-15 16:35:15 +0100

    Adding User Registration Functionalities

--- src/main/java/com/somupvotes/web/LoginController.java
+++ src/main/java/com/somupvotes/web/LoginController.java
@@ -1,13 +1,40 @@
 package com.somupvotes.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.somupvotes.domain.User;
+import com.somupvotes.service.UserService;
 
 @Controller
 public class LoginController {
 
+	@Autowired
+	private UserService userService;
+	
 @GetMapping("/login")
 public String login(){
 	return "login";
+}
+
+@GetMapping("/register")
+public String register(ModelMap model){
+	model.put("user", new User());
+	
+	return "register";
+}
+
+@PostMapping("/register")
+public String registerAccount(@ModelAttribute User user) {
+	  
+	User savedUser =  userService.save(user);
+//	System.out.println(savedUser);
+//	System.out.println(user);
+	
+	return "redirect:/login";
 }
 }
